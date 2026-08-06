6078d4c8d3d00bd555f379f01d807e1ea37bd6f2
FajarNaeem01 <fajarnaeem010@gmail.com>
2026-08-06 21:24:04 +0500

    Create Smart Profile Analyzer project

---
+++ Day01_Python_Fundamentals/profile_analyzer.py
@@ -0,0 +1,15 @@
+"""
+===============================================
+Day 01 - Python Fundamentals
+Mini Project: Profile Analyzer
+Description: This program analyzes a user's profile information and provides insights based on the data provided.
+Author: Fajar Naeem Rana
+===============================================
+"""
+student_name = input("Enter your name: ")
+student_age = int(input("Enter your age: "))
+university_name = input("Enter your university name: ")
+department_name = input("Enter your department name: ")
+email_address = input("Enter your email address: ")
+programming_languages = input("Enter your favorite programming languages (comma-separated): ").split(',')
+cgpa = float(input("Enter your CGPA: "))
