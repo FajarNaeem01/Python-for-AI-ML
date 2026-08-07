98aadaf1c1568d1c1f145060ece2ae26f8c51e0d
FajarNaeem01 <fajarnaeem010@gmail.com>
2026-08-07 17:24:00 +0500

    Sets practice

--- Day02_Python_Collections/practice.py
+++ Day02_Python_Collections/practice.py
@@ -69,3 +69,41 @@ print(f"Major: {major}")
 print(f"CGPA: {cgpa}")
 # immutable nature of tuples
 # student[0] = "John Doe"  # This will raise an error
+
+#===========================================
+# 3. SETS
+#===========================================
+languages = {"Python", "C++", "Java", "Python", "Java"}
+print(f"Languages: {languages}")
+print(f"Number of unique languages: {len(languages)}")
+languages.add("JavaScript")
+languages.update(["C", "Rust"])
+languages.discard("Java")
+languages.pop()  # removes an arbitrary element
+print(f"Languages after modifications: {languages}")
+
+#unique values from a list
+numbers = [1, 2, 3, 2, 4, 1, 5, 3, 6, 5]
+print(f"Unique Numbers: {set(numbers)}")
+
+# set operations
+python_students = {"Ali", "Sara", "Fajar", "Ahmed"}
+ml_students = {"Fajar", "Ahmed", "Usman", "Ayesha"}
+
+print(("Course Analysis:"))
+print(f"Total Students: {len(python_students.union(ml_students))}")
+print(f"Students in both courses: {python_students.intersection(ml_students)}")
+print(f"Students only in Python course: {python_students.difference(ml_students)}")
+print(f"Students only in ML course: {ml_students.difference(python_students)}")
+print(f"Students enrolling in only one course: {python_students.symmetric_difference(ml_students)}")
+
+# telling if a student is enrolled in a course
+name=input("Enter your name: ")
+if name in python_students:
+    print(f"Hello {name}, you are enrolled in the Python course.")
+elif name in ml_students:
+    print(f"Hello {name}, you are enrolled in the ML course.")
+elif name in python_students.union(ml_students):
+    print(f"Hello {name}, you are enrolled in both courses.")
+else:
+    print(f"Hello {name}, you are not enrolled in either course.")
