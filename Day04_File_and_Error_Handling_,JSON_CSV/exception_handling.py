7001e547974085ef3a9642aa464da823d5e8e336
FajarNaeem01 <fajarnaeem010@gmail.com>
2026-08-10 23:16:51 +0500

    Exception Handling practice

---
+++ Day04_File_and_Error_Handling_,JSON_CSV/exception_handling.py
@@ -0,0 +1,54 @@
+"""
+===============================================
+Python for AI/ML Engineering
+Phase 0 - Professional Python
+
+Day 04 - Exception Handling
+Practice File
+
+Author: Fajar Naeem Rana
+===============================================
+"""
+
+# Safe Division
+# ========================
+print("Enter two numbers to divide")
+try:
+    number1=float(input("First number: "))
+    number2=float(input("second number: "))
+    print(f"Result: {number1/number2}")
+
+except ZeroDivisionError:
+    print("Division by zero")
+
+except ValueError:
+    print("Invalid input")
+
+else:
+    print("Numbers divided withour error")
+
+finally:
+    print("Division operation completed")
+
+# Safe File Handling
+# ==============================
+try:
+    with open(r"Day04_File_and_Error_Handling_,JSON_CSV\data\students.txt", "r") as file:
+        student_data=file.read()
+
+except FileNotFoundError as fileError:
+    print("Something went wrong: ", {fileError})
+
+else:
+    print(student_data)
+
+# Student CGPA Validator- Raising your own exceptions
+# =================================
+cgpa=float(input("enter your gpa: "))
+
+if cgpa < 0:
+    raise ValueError("CGPA cannot be negative")
+elif cgpa > 4:
+    raise ValueError("CGPA can be from 0-4")
+else:
+    print(f"Your cgpa is {cgpa}")
