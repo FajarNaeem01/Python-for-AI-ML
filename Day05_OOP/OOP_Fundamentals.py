564d45caf9038ca078409f8590a8e0de7d0b7899
FajarNaeem01 <fajarnaeem010@gmail.com>
2026-08-11 22:47:52 +0500

    Initial Commit: Day 05- Python OOP

---
+++ Day05_OOP/OOP_Fundamentals.py
@@ -0,0 +1,11 @@
+"""
+===============================================
+Python for AI/ML Engineering
+Phase 0 - Professional Python
+
+Day 04 - CSV
+Practice File
+
+Author: Fajar Naeem Rana
+===============================================
+"""
