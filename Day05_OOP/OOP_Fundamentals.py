8b0d93b8be5fa2df89a70914866dd51349e024b4
FajarNaeem01 <fajarnaeem010@gmail.com>
2026-08-12 02:00:50 +0500

    Encapsulation

--- Day05_OOP/OOP_Fundamentals.py
+++ Day05_OOP/OOP_Fundamentals.py
@@ -9,73 +9,82 @@ Practice File
 Author: Fajar Naeem Rana
 ===============================================
 """
+#==============================================
+#            BankAccount Class 
+#==============================================
 class BankAccount:
     bank_name = "HBL "
     def __init__(self,account_holder, account_number, balance):
-        self.account_holder = account_holder
+        self.account_holder = account_holder   # attribute initilization
         self.account_number = account_number
-        self.balance = balance
+        self.__balance = balance               # private access
 
     def display_account(self):
         print("\n=========== Account Summary ============")
-        print(f"Bank Name: {BankAccount.bank_name}")
+        print(f"Bank Name: {BankAccount.bank_name}")   # accessing class attribute
         print(f"Account Holder: {self.account_holder}")
         print(f"Account Number: {self.account_number}")
-        print(f"Current Balance: {self.balance}")
-        print(f"Account Active: {self.is_active()}")
+        print(f"Current Balance: {self.__balance}")
+        print(f"Account Active: {self.is_active()}")   # calling other method
         print("=========================================")
 
     def deposit(self, amount):
-        self.balance += amount
+        if amount <=0:
+            print("Invalid! Deposit Rejected")
+        self.__balance += amount     # changing object's state
         return amount
 
     def withdraw(self,amount):
-        if amount > self.balance:
-            
-            return False
+
+        if amount > self.__balance or amount < 0:
+            return False, amount
         else:
-            self.balance -= amount
-            return True
+            self.__balance -= amount
+            return True, amount  # returning multiple variables
 
     def current_balance(self):
-        return self.balance
+        return self.__balance
 
     def is_active(self):
-        if self.balance >= 0:
+        if self.__balance >= 0:
             return True
         else:
             return False
 
     def transfer(self, other_account, amount):
-        self.balance -= amount
+        if amount > self.__balance or amount <= 0:
+            print("Invalid! Transfer Rejected.")
+        self.__balance -= amount
         other_account.deposit(amount)
         return amount
 
+# =============================================
+# ============= Creating objects ==============
 account1 = BankAccount("Fajar", "PK001", 5000)
 account2 = BankAccount("Noor", "PK002", 10000)
 
+print("Accounts detail before any operation:")
 account1.display_account()
 account2.display_account()
 
+# ============== Deposit ===============
 amount= account1.deposit(4000)
 print(f"\n{amount} Deposited!")
 print(f"Current Balance: {account1.current_balance()}")
 
+# ============= Transfer ================
 amount= account1.transfer(account2, 3000)
 print(f"{amount} transferred to {account2.account_holder}")
 
-if account1.withdraw(1500):
-    print("\nRs 1500 Withdrawn!")
-    print(f"Current Balance: {account1.current_balance()}")
+# ============= withdrawal ===============
+amount = float(input("Enter amount to withdraw: "))
+is_withdrawn, amount_withdrawn = account1.withdraw(amount)
+if is_withdrawn:
+    print(f"{amount_withdrawn} is withdrawn")
 else:
-    print("\nLow Balance! Cannot withdraw money.")
+    print("Invalid! Withdrawal Rejected")
 
-if account1.withdraw(10000):
-    print("\nRs 10,000 Withdrawn!")
-    print(f"Current Balance: {account1.current_balance()}")
-else:
-    print("\nLow Balance! Cannot withdraw money.")
-
+# ====== changing class atrribute ========
 BankAccount.bank_name= "HBL Bank"
 account1.display_account()
 account2.display_account()
